0ae6cca49bb91c808ed6941a7cad71b528d4da12
sachinnair26 <sachin.nair32@gmail.com>
2018-09-23 21:54:51 +0530

    timetable

--- src/Components/Calender/index.js
+++ src/Components/Calender/index.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
-import "./index.css"
-import { Calendar, Badge } from 'antd';
+import React, { Component } from "react";
+import "./index.css";
+import { Calendar, Badge } from "antd";
 
 function getListData(value) {
   let listData;
   switch (value.date()) {
     case 8:
-      listData = [
-        { type: 'warning', content: 'This is warning event.' },
-      ]; break;
+      listData = [{ type: "warning", content: "This is warning event." }];
+      break;
     case 10:
-      listData = [
-        { type: 'warning' },
-        
-      ]; break;
+      listData = [{ type: "warning" }];
+      break;
     case 15:
-      listData = [
-        { type: 'warning' },
-      ]; break;
+      listData = [{ type: "warning" }];
+      break;
     default:
   }
   return listData || [];
@@ -27,44 +23,31 @@ function dateCellRender(value) {
   const listData = getListData(value);
   return (
     <ul className="events">
-      {
-        listData.map(item => (
-          <li key={item.content}>
-            <Badge status="success" />
-          </li>
-        ))
-      }
+      {listData.map(item => (
+        <li key={item.content}>
+          <Badge status="success" />
+        </li>
+      ))}
     </ul>
   );
 }
 
+export default class extends Component {
+  constructor(props) {
+    super(props);
+  }
 
+  componentDidMount() {}
 
-
-export default class  extends Component {
-
-    constructor(props){
-      super(props)
-    
-      
-    }
-    
-    componentDidMount(){
-    
-    
-    }
-    
-    
-      render() {
-    
-       
-    
-        return (
-          <div className="common-container" >
-   <div style={{ width: 300, border: '1px solid #d9d9d9', borderRadius: 4 }}>
-    <Calendar fullscreen={false}  dateCellRender={dateCellRender} />
-  </div>
-          </div>
-        );
-      }
-    }
+  render() {
+    return (
+      <div className="common-container">
+        <div
+          style={{ width: 300, border: "1px solid #d9d9d9", borderRadius: 4 }}
+        >
+          <Calendar fullscreen={false} dateCellRender={dateCellRender} />
+        </div>
+      </div>
+    );
+  }
+}
