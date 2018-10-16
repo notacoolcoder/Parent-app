7be475ce143bf0f919d1f69ff72bfce3553f5a52
Adhil Juvane <adhilmp3@gmail.com>
2018-10-16 21:36:36 +0530

    modal

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import AppBar from "material-ui/AppBar";
-
+import Dialog from "material-ui/Dialog";
 import Drawer from "material-ui/Drawer";
-
+import { RadioButton, RadioButtonGroup } from "material-ui/RadioButton";
+import FlatButton from "material-ui/FlatButton";
 import { Icon } from "antd";
+
+const styles = {
+  block: {
+    maxWidth: 250
+  },
+  radioButton: {
+    marginBottom: 16
+  }
+};
 
 export default class extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false
+      open: false,
+      openModal : false ,
     };
     this.style = {
       menuItem: {
@@ -25,32 +36,83 @@ export default class extends Component {
     };
   }
 
+
   handleToggle = () => this.setState({ open: !this.state.open });
 
   handleClose = () => this.setState({ open: false });
 
+  handleClose = () => {
+    this.setState({ openModal : false });
+  };
+
+  onIconClick = () => {
+    this.setState({
+      openModal : true ,
+    })
+  }
+
   render() {
+    const actions = [,];
     return (
       <div>
         <AppBar
           style={{ backgroundColor: "rgb(53, 53, 110)" }}
           title="Parent Connect"
-          onClick={this.handleToggle}
+          onLeftIconButtonClick={this.handleToggle}
           iconElementRight={
             <Icon
               style={{
                 width: "100%",
                 height: "80%",
                 fontSize: "25px",
+                color : "white",
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center"
               }}
               type="user"
               theme="outlined"
+              onClick={this.onIconClick}
             />
           }
         />
+        <Dialog
+          actions={actions}
+          modal={false}
+          open={this.state.openModal}
+          onRequestClose={this.handleClose}
+        >
+          <RadioButtonGroup name="shipSpeed" defaultSelected="not_light">
+            <RadioButton
+              value="light"
+              label="Dhanista Sudhir Katuparampil, 10-F"
+              style={styles.radioButton}
+            />
+            <RadioButton
+              value="not-light"
+              label="Keerthy, 5-A"
+              style={styles.radioButton}
+            />
+            <RadioButton
+              value="not-light1"
+              label="Khyati, 5-A"
+              style={styles.radioButton}
+            />
+            <RadioButton
+              value="not-light2"
+              label="Kiran, 5-A"
+              style={styles.radioButton}
+            />
+          </RadioButtonGroup>
+
+          <FlatButton
+            style={{ margin: "auto", backgroundColor: "#fff44f " }}
+            label="Submit"
+            primary={true}
+            keyboardFocused={true}
+            onClick={this.handleClose}
+          />
+        </Dialog>
         <Drawer
           containerStyle={{
             backgroundColor: "rgb(53, 53, 110)",
