1260c4abd75fefc433c76f9f2bb36f79373ea872
akshaycj <akshaycj999@gmail.com>
2018-11-10 16:51:43 +0530

    Updated count for classtest

--- src/Components/MessagesTab/ClassTest/index.js
+++ src/Components/MessagesTab/ClassTest/index.js
@@ -29,7 +29,7 @@ export default class extends Component {
     var data = {
       studentId: this.state.studentId,
       offset: 0,
-      count: 5
+      count: 10
     };
     fetch(baseUrl + "dailyreport/test/countwise/", {
       method: "POST",
