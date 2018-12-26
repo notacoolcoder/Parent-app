d3c717e58f45edf9fca03878419337dffc1d826d
akshaycj <akshaycj999@gmail.com>
2018-12-26 07:41:45 +0530

    Gallery Slider implemented

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,5 @@
 export const baseUrl = "https://parentconnect.in/ssdiary/parentApp/";
 export const anotherBaseUrl = "https://parentconnect.in/ssdiary/parentapp/";
 export const galleryAPI =
-  "https://appscook.s3-ap-southeast-1.amazonaws.com/?delimiter=/&prefix=ssd/schoolGallery/";
-export const galleryBaseUrl =
-  "https://appscook.s3-ap-southeast-1.amazonaws.com/";
+  "https://appscook.ams3.digitaloceanspaces.com/?delimiter=/&prefix=ssd/schoolGallery/";
+export const galleryBaseUrl = "https://appscook.ams3.digitaloceanspaces.com/";
--- src/Components/GallerySub/index.js
+++ src/Components/GallerySub/index.js
@@ -1,28 +1,28 @@
-import React from "react" ;
-import "./index.css" ;
-import { galleryAPI } from "./../../Api";
+import React from "react";
+import "./index.css";
+import { galleryAPI, galleryBaseUrl } from "./../../Api";
 import { parseString } from "xml2js";
 import GalleryBlock from "./../Gallery/GalleryBlock";
-import {Link} from 'react-router-dom'
-import {Icon} from 'antd'
-
+import { Link } from "react-router-dom";
+import { Icon } from "antd";
+import Gallery from "react-grid-gallery";
 
 export default class GallerySub extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      floderName : "" ,
+      floderName: "",
       schoolCode: localStorage.getItem("schoolID"),
-      gallery : [] ,
-
-    }
+      gallery: [],
+      images: []
+    };
   }
 
-  componentDidMount(){
-    var folder = this.props.match.params.folder ;
+  componentDidMount() {
+    var folder = this.props.match.params.folder;
     var that = this;
     const schoolCode = this.state.schoolCode;
-    fetch(galleryAPI + schoolCode + "/"+ folder + "/")
+    fetch(galleryAPI + schoolCode + "/" + folder + "/")
       .then(respose => {
         console.log("res", respose);
         return respose.text();
@@ -31,34 +31,74 @@ export default class GallerySub extends React.Component {
       .then(value => {
         parseString(value, { explicitArray: false }, (err, result) => {
           //console.log("Res", result.ListBucketResult.Contents);
-          console.log("dat",result);
-          const images = result.ListBucketResult.Contents;
-          var dat =[]
-          images.length>1?(dat = images):dat.push(images)
-          that.setState({ gallery: dat });
-          console.log("dat",dat);
+          console.log("dat---", result);
+          try {
+            const images = result.ListBucketResult.Contents;
+            var dat = [];
+            images.length > 1 ? (dat = images) : dat.push(images);
+
+            that.setState({ gallery: dat });
+
+            console.log("dat", dat);
+            this.getImages(dat);
+          } catch (err) {}
         });
       });
   }
 
-  render(){
-    return(
+  getImages = data => {
+    const images = [];
+    data.map((item, index) => {
+      // index > 0
+      //   ? images.push({
+      //       src: galleryBaseUrl + item.Key,
+      //       thumbnail: galleryBaseUrl + item.Key,
+      //       thumbnailWidth: 320,
+      //       thumbnailHeight: 174
+      //     })
+      //   : null;
+      images.push({
+        src: galleryBaseUrl + item.Key,
+        thumbnail: galleryBaseUrl + item.Key,
+        thumbnailWidth: 80,
+        thumbnailHeight: 80
+      });
+    });
+    this.setState({ images });
+  };
+
+  render() {
+    return (
       <div>
-        <Link to="/gallery" >
-      <div  style={{display : "flex" , flexDirection : "row"}}>
-              <Icon style={{fontSize : "35px" , margin : "10px" , color : "#08c"}} type="arrow-left" />
-              <div style={{display : "flex" , alignItems : "center"}}>Go back</div>
+        <Link to="/gallery">
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <Icon
+              style={{ fontSize: "35px", margin: "10px", color: "#08c" }}
+              type="arrow-left"
+            />
+            <div style={{ display: "flex", alignItems: "center" }}>Go back</div>
           </div>
-      </Link>
-      <div style={{ padding: "5px", display: "flex", flexWrap: "wrap", margin: "auto", justifyContent: "center" }}>
-      {this.state.gallery.map( item => (
-        <GalleryBlock url={item.Key}/>
-      ))}
-      {/* {this.state.data.map(s => (
-        <GalleryCard date={s.date} gallery={s.gallery} />
-      ))} */}
+        </Link>
+        {this.state.images.length > 0 ? (
+          <Gallery images={this.state.images} />
+        ) : (
+          <div style={{ textAlign: "center" }}>No data!</div>
+        )}
+        {/* <div
+          style={{
+            padding: "5px",
+            display: "flex",
+            flexWrap: "wrap",
+            margin: "auto",
+            justifyContent: "center"
+          }}
+        >
+          {this.state.gallery.map(item => (
+            <GalleryBlock url={item.Key} />
+          ))}
+          
+        </div> */}
       </div>
-      </div>
-    )
+    );
   }
 }
